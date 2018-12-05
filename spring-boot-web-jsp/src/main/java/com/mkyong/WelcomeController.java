390e6d47ad516e89c16b601cd58542e4f2f43606
ariad <ariad@192.168.1.13>
2018-12-04 22:30:27 -0500

    Conexionjsp.

--- spring-boot-web-jsp/src/main/java/com/mkyong/WelcomeController.java
+++ spring-boot-web-jsp/src/main/java/com/mkyong/WelcomeController.java
@@ -9,21 +9,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController {
 
-	// inject via application.properties
-	@Value("${welcome.message:test}")
-	private String message = "Hello World";
+	
+// inject via application.properties
+	
+@Value("${welcome.message:test}")
+	
+private String message = "Hello World";
 
-	@RequestMapping("/")
-	public String welcome(Map<String, Object> model) {
-		model.put("message", this.message);
-		return "index";
+	
+@RequestMapping("/")
+	
+public String welcome(Map<String, Object> model) {
+		
+model.put("message", this.message);
+		
+return "index";
 	}
-	@RequestMapping("categories")
-	public String categories() {
-		return "categories";
+	
+@RequestMapping("categories")
+	
+public String categories() {
+		
+return "categories";
 	}
-	
-	
-	
-	
+	@RequestMapping("cart")
+	public String cart() {
+		return "cart";
+	}
+	@RequestMapping("contact")
+	public String contact() {
+		return "contact";
+	}
+	@RequestMapping("product")
+	public String product() {
+		return "product";
+	}
+
 }
